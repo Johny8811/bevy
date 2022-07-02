e3c76ec1b35673a7b1cb43150d92f6add716abef
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-02 19:59:58 +0200

    Adjust file structure, remove redundant code, install and setup base development packages

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { red } from '@mui/material/colors';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#318260'
+    },
+    secondary: {
+      main: '#b72548'
+    },
+    error: {
+      main: red.A400
+    }
+  }
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
