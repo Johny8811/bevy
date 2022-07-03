605a5f51c9c27405f26ca52255fade915c88b0a1
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-03 19:53:02 +0200

    Add dashboard bar

--- src/main/modules/dashboard/Dashboard.tsx
+++ src/main/modules/dashboard/Dashboard.tsx
@@ -1,5 +1,22 @@
-import React from 'react';
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 export function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Dispatcher: Emil
+          </Typography>
+          <Button color="inherit">Export data</Button>
+          <Button color="inherit">Load data</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
 }
