c22cf971c128fae01de52a76d564926ed7afe422
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-03 12:29:57 +0200

    Add no matching route

--- src/Routing.tsx
+++ src/Routing.tsx
@@ -3,12 +3,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { SignIn } from './main/modules/signIn/SignIn';
 import { Dashboard } from './main/modules/dashboard/Dashboard';
 
+import { NoMatchingRoute } from './main/components/NoMatchingRoute';
+
 function Routing() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SignIn />} />
         <Route path="dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NoMatchingRoute />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/main/components/NoMatchingRoute.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function NoMatchingRoute() {
+  return (
+    <main style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <p>There&apos;s nothing here!</p>
+    </main>
+  );
+}
