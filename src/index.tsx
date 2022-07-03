a2773f3ccd980805a125f425496843d6574c62c2
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-03 21:23:56 +0200

    Add packages for wirk with tables and date pickers and initialization

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { red } from '@mui/material/colors';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import Routing from './routing/Routing';
 import reportWebVitals from './reportWebVitals';
+// TypeScript 4.x and above
+import type {} from '@mui/x-date-pickers/themeAugmentation';
 
 const theme = createTheme({
   palette: {
@@ -24,7 +28,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Routing />
+      <LocalizationProvider dateAdapter={AdapterDateFns}>
+        <Routing />
+      </LocalizationProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
