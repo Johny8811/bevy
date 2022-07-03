abae99d7c3bdfde8226df86ae2ad8e4be08e7268
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-03 09:19:46 +0200

    Rename

--- src/app/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import './App.css';
-
-function App() {
-  return <div>Main page</div>;
-}
-
-export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { red } from '@mui/material/colors';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import App from './app/App';
+import { BrowserRouter } from 'react-router-dom';
+import App from './main/App';
 import reportWebVitals from './reportWebVitals';
 
 const theme = createTheme({
@@ -24,7 +25,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
