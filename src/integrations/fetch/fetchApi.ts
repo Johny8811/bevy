3ebf921cd2ffb7fc2a856bfee8441db042263929
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-19 12:26:56 +0200

    Implement fetch api, fix eslint rules problems

---
+++ src/integrations/fetch/fetchApi.ts
@@ -0,0 +1,35 @@
+export enum Methods {
+  get = 'GET',
+  post = 'POST'
+}
+
+export const fetchApi = async ({
+  url,
+  method = Methods.get,
+  headers,
+  body
+}: {
+  url: string;
+  method?: Methods;
+  headers?: { [key: string]: string };
+  // TODO: improve typing
+  body?: any;
+}) => {
+  switch (method) {
+    case Methods.get: {
+      const response = await fetch(url, { method: Methods.get, headers });
+      await response.json();
+      return response;
+    }
+    case Methods.post: {
+      const response = await fetch(url, { method: Methods.post, body, headers });
+      await response.json();
+      return response;
+    }
+    default: {
+      return new Promise((resolve, reject) => {
+        reject(new Error('Fetch method has to be set.'));
+      });
+    }
+  }
+};
