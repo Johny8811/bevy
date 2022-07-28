79322b786ba736f5cbe06950a99b27019862384a
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-28 13:13:27 +0200

    Add parse street util

---
+++ src/main/utils/parseStreet.ts
@@ -0,0 +1,20 @@
+// TODO: test
+export const parseStreet = (data: string) => {
+  const streetItems = data.split(' ');
+  const lengthOfStreetItems = streetItems.length;
+
+  const street = streetItems
+    .map((item, index) => {
+      if (index === lengthOfStreetItems - 1) {
+        return undefined;
+      }
+
+      return item;
+    })
+    .join(' ');
+
+  return {
+    street,
+    streetNo: streetItems[lengthOfStreetItems - 1]
+  };
+};
