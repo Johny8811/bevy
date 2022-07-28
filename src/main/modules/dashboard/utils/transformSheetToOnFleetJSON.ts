d8055bb58438381dcac6925ed9b435a1e60cfd86
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-28 13:57:56 +0200

    Move and improve "transform sheet to onFleet" util

--- src/main/modules/dashboard/utils/transformSheetToOnFleetJSON.ts
+++
@@ -1,9 +0,0 @@
-import { read, utils } from 'xlsx';
-
-export const transformSheetToOnFleetJSON = async (tasksXlsx: File) => {
-  const buff = await tasksXlsx.arrayBuffer();
-  const workbook = read(buff);
-
-  const sheetNameList = workbook.SheetNames;
-  console.log('==> JSON: ', utils.sheet_to_json(workbook.Sheets[sheetNameList[0]]));
-};
---
+++ src/main/utils/transformSheeToOnFleet/index.ts
@@ -0,0 +1,44 @@
+import { read, utils } from 'xlsx';
+import { parseStreet } from '../parseStreet';
+import { RawSheetData, SheetColumns } from './types';
+
+import { excelDateToJSDate } from '../excelDateToJSDate';
+
+export const transformSheetToOnFleet = async (tasksXlsx: File) => {
+  const buff = await tasksXlsx.arrayBuffer();
+  const workbook = read(buff);
+
+  const sheetNameList = workbook.SheetNames;
+  const parsedSheetData = utils.sheet_to_json<RawSheetData>(workbook.Sheets[sheetNameList[0]]);
+
+  const onFleetTasks = parsedSheetData.map((data) => {
+    const { street, streetNo } = parseStreet(data[SheetColumns.ADDRESS_STREET]);
+    const name = `${data[SheetColumns.CUSTOMER_NAME]} ${data[SheetColumns.QUANTITY]}`;
+
+    return {
+      destination: {
+        address: {
+          number: streetNo,
+          street,
+          city: data[SheetColumns.COUNTRY],
+          postalCode: data[SheetColumns.POSTAL_CODE],
+          // TODO: has to be added
+          country: 'Czech Republic'
+          // country: data[SheetColumns]
+        }
+      },
+      recipients: [
+        {
+          name,
+          phone: data[SheetColumns.TEL_NUMBER],
+          notes: data[SheetColumns.CUSTOMER_NOTE],
+          skipSMSNotifications: data[SheetColumns.NOTIFICATION]
+        }
+      ],
+      completeAfter: excelDateToJSDate(data[SheetColumns.DELIVER_AFTER]),
+      completeBefore: excelDateToJSDate(data[SheetColumns.DELIVER_BEFORE])
+    };
+  });
+
+  console.log('==> onFleetTasks: ', onFleetTasks);
+};
