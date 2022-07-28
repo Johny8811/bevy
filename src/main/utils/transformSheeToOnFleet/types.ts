c380318007084271415592d3b55a117a7102cc8b
Jan Hujsa <jan.hujsa@gmail.com>
2022-07-28 13:20:03 +0200

    Add types of transformation sheet to onfleet tasks util

---
+++ src/main/utils/transformSheeToOnFleet/types.ts
@@ -0,0 +1,33 @@
+export enum SheetColumns {
+  CUSTOMER_NAME = 'Customer_name',
+  TEL_NUMBER = 'Tel_number',
+  NOTIFICATION = 'Notification',
+  CUSTOMER_NOTE = 'Customer_note',
+  ADDRESS_STREET = 'Address_street',
+  POSTAL_CODE = 'Postal_code',
+  COUNTRY = 'Country',
+  DELIVER_AFTER = 'Deliver_after',
+  DELIVER_BEFORE = 'Deliver_before',
+  QUANTITY = 'Quantity',
+  // currently not used
+  PAYMENT = 'Payment',
+  CASH_ON_DELIVER = 'Cash_on_deliver',
+  INTERNAL_ORDER_NO = 'Internal_order_no'
+}
+
+export type RawSheetData = {
+  [SheetColumns.CUSTOMER_NAME]: string;
+  [SheetColumns.TEL_NUMBER]: number;
+  [SheetColumns.NOTIFICATION]: boolean;
+  [SheetColumns.CUSTOMER_NOTE]: string;
+  [SheetColumns.ADDRESS_STREET]: string;
+  [SheetColumns.POSTAL_CODE]: number;
+  [SheetColumns.COUNTRY]: string;
+  [SheetColumns.DELIVER_AFTER]: number;
+  [SheetColumns.DELIVER_BEFORE]: number;
+  [SheetColumns.QUANTITY]: number;
+  // -- define types of these items
+  [SheetColumns.PAYMENT]: number;
+  [SheetColumns.CASH_ON_DELIVER]: number;
+  [SheetColumns.INTERNAL_ORDER_NO]: number;
+};
