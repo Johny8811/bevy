1e958521915c027f4ad06377df133f147b21ed81
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-16 23:37:19 +0200

    Add table with fake data

--- src/main/modules/dashboard/components/BadImportsTable.tsx
+++ src/main/modules/dashboard/components/BadImportsTable.tsx
@@ -5,8 +5,53 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+// import Box from '@mui/material/Box';
 
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
 import { isDev } from '../../../utils/isDev';
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 90 },
+  {
+    field: 'firstName',
+    headerName: 'First name',
+    width: 150,
+    editable: true
+  },
+  {
+    field: 'lastName',
+    headerName: 'Last name',
+    width: 150,
+    editable: true
+  },
+  {
+    field: 'age',
+    headerName: 'Age',
+    type: 'number',
+    width: 110,
+    editable: true
+  },
+  {
+    field: 'fullName',
+    headerName: 'Full name',
+    description: 'This column has a value getter and is not sortable.',
+    sortable: false,
+    width: 160,
+    valueGetter: ({ row }: GridValueGetterParams) => `${row.firstName || ''} ${row.lastName || ''}`
+  }
+];
+
+const rows = [
+  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
+  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
+  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
+  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
+  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
+  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
+  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
+  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
+  { id: 9, lastName: 'Prd', firstName: 'Harvey', age: 65 }
+];
 
 export function BadImportsTable() {
   const descriptionElementRef = useRef<HTMLElement>(null);
@@ -46,12 +91,19 @@ export function BadImportsTable() {
         <DialogTitle id="scroll-dialog-title">
           Some tasks was not imported because of missing or bad data
         </DialogTitle>
-        <DialogContent dividers>
+        <DialogContent>
           <DialogContentText
             id="scroll-dialog-description"
             ref={descriptionElementRef}
             tabIndex={-1}>
-            Here will be table
+            <DataGrid
+              rows={rows}
+              columns={columns}
+              // checkboxSelection
+              disableSelectionOnClick
+              autoHeight
+              hideFooter
+            />
           </DialogContentText>
         </DialogContent>
         <DialogActions>
