12bc118f1f1601680aeceb48306480fec5009e69
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-16 19:16:02 +0200

    Add firebase dev configuration

---
+++ src/main/integrations/firebase/constants.ts
@@ -0,0 +1,11 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfigDev = {
+  apiKey: 'AIzaSyBbUkEoHXiW77RVnKmT8iiBvJKCiuQ0E70',
+  authDomain: 'bevy-bbf67.firebaseapp.com',
+  projectId: 'bevy-bbf67',
+  storageBucket: 'bevy-bbf67.appspot.com',
+  messagingSenderId: '939982424969',
+  appId: '1:939982424969:web:647ce5424c0de9d77f6f58',
+  measurementId: 'G-ZKECYLRWPJ'
+};
--- src/main/integrations/firebase/index.ts
+++ src/main/integrations/firebase/index.ts
@@ -4,9 +4,16 @@ import { getAuth } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { firebaseConfig } from './constants';
+import { isDev } from '../../utils/isDev';
+
+import { firebaseConfigDev } from './constants';
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(
+  isDev()
+    ? // TODO: add production config
+      firebaseConfigDev
+    : firebaseConfigDev
+);
 export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
