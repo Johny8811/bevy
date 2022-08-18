0a5fd887d9e3473123e20b8205d26eb56bdb4d76
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-18 18:58:33 +0200

    Additional transform and validate data

--- src/main/modules/dashboard/components/BadImportsDialog/TableFooter.tsx
+++ src/main/modules/dashboard/components/BadImportsDialog/TableFooter.tsx
@@ -4,11 +4,20 @@ import Button from '@mui/material/Button';
 import { useGridApiContext } from '@mui/x-data-grid';
 
 import { getGridData } from '../../../../utils/dataGrid/getGridData';
+import { badImportsValidationSchema } from '../../validation/validationSchema';
 import { Props as TableProps } from './Table';
 
 export function TableFooter({ onConfirm, onCancel }: Omit<TableProps, 'failedTasks'>) {
   const apiRef = useGridApiContext();
   const data = getGridData(apiRef);
+  // TODO: cell should be "date" type
+  const transformData = data.map((d) => {
+    return {
+      ...d,
+      completeAfter: new Date(d.completeAfter).getTime(),
+      completeBefore: new Date(d.completeBefore).getTime()
+    };
+  });
 
   return (
     <Box sx={{ p: 1, display: 'flex', justifyContent: 'end' }}>
@@ -18,9 +27,16 @@ export function TableFooter({ onConfirm, onCancel }: Omit<TableProps, 'failedTas
       <Button
         variant="contained"
         onClick={() => {
-          // TODO: add validation
-          // @ts-ignore
-          onConfirm(data);
+          badImportsValidationSchema
+            .validate(transformData)
+            .then((validationResult) => {
+              if (validationResult) {
+                onConfirm(validationResult);
+              }
+            })
+            .catch((e) => {
+              console.log('=-=> validation ERRor: ', e);
+            });
         }}>
         Import fixed tasks
       </Button>
