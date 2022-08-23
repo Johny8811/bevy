a7e941927aa7ed5b5cfcc165586d135491b7500e
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-23 22:08:13 +0200

    Transform tasks for showing in table

--- src/main/modules/dashboard/components/DeliveryTable/Table.tsx
+++ src/main/modules/dashboard/components/DeliveryTable/Table.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import { DataGrid } from '@mui/x-data-grid';
-import { OnfleetTask } from '@onfleet/node-onfleet/Resources/Tasks';
 
 import { useUserTasksQuery } from '../../../../queryHooks/useUserTasksQuery';
+import { TaskData } from '../../../../types/tasks';
 import { useUser } from '../../../../integrations/firebase/components/UserProvider';
 import { useSnackBar } from '../../../../components/snackBar/SnackbarProvider';
 import { DELIVERY_COLUMNS } from '../../constants/columns';
@@ -17,15 +17,34 @@ export function Table({ selectedDay }: Props) {
   const { user } = useUser();
   const userTasksQuery = useUserTasksQuery();
   const { openSnackBar } = useSnackBar();
-  const [userTasks, setUserTasks] = useState<OnfleetTask[] | []>([]);
+  const [userTasks, setUserTasks] = useState<
+    | ({ id: string } & Pick<
+        TaskData,
+        'name' | 'phoneNumber' | 'street' | 'houseNumber' | 'city' | 'country' | 'quantity'
+      >)[]
+    | []
+  >([]);
 
   useEffect(() => {
     if (user?.uid && selectedDay) {
       userTasksQuery(user?.uid, selectedDay)
-        .then((tasks) => setUserTasks(tasks))
-        .catch(() => {
+        .then((tasks) =>
+          setUserTasks(
+            tasks.map((task) => ({
+              id: task.id,
+              name: task.recipients[0]?.name,
+              phoneNumber: task.recipients[0]?.phone,
+              street: task.destination.address.street,
+              houseNumber: task.destination.address.number,
+              city: task.destination.address.city,
+              country: task.destination.address.country,
+              quantity: task.quantity
+            }))
+          )
+        )
+        .catch((e) => {
           openSnackBar({
-            text: 'Someting went wrong while getting data',
+            text: `Something went wrong while getting data - ${e.message}`,
             severity: 'error'
           });
         });
