58d20ee3a98e235cbafef02918355e21692bcac7
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-23 22:03:57 +0200

    Move columns to constants file

--- src/main/modules/dashboard/components/DeliveryTable/Table.tsx
+++ src/main/modules/dashboard/components/DeliveryTable/Table.tsx
@@ -1,43 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { DataGrid } from '@mui/x-data-grid';
 import { OnfleetTask } from '@onfleet/node-onfleet/Resources/Tasks';
 
 import { useUserTasksQuery } from '../../../../queryHooks/useUserTasksQuery';
 import { useUser } from '../../../../integrations/firebase/components/UserProvider';
 import { useSnackBar } from '../../../../components/snackBar/SnackbarProvider';
+import { DELIVERY_COLUMNS } from '../../constants/columns';
 import { NoData } from './NoData';
-
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'firstName',
-    headerName: 'First name',
-    width: 150,
-    editable: true
-  },
-  {
-    field: 'lastName',
-    headerName: 'Last name',
-    width: 150,
-    editable: true
-  },
-  {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
-    width: 110,
-    editable: true
-  },
-  {
-    field: 'fullName',
-    headerName: 'Full name',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-    valueGetter: ({ row }: GridValueGetterParams) => `${row.firstName || ''} ${row.lastName || ''}`
-  }
-];
 
 type Props = {
   selectedDay: Date | null;
@@ -66,7 +36,7 @@ export function Table({ selectedDay }: Props) {
     <Box sx={{ height: 'calc(100vh - 68px)', width: '100%' }}>
       <DataGrid
         rows={userTasks}
-        columns={columns}
+        columns={DELIVERY_COLUMNS}
         checkboxSelection
         disableSelectionOnClick
         components={{ NoRowsOverlay: NoData }}
--- src/main/modules/dashboard/constants/columns.ts
+++ src/main/modules/dashboard/constants/columns.ts
@@ -20,3 +20,13 @@ export const BAD_IMPORTS_COLUMNS: GridColDef[] = [
   { field: 'completeBefore', headerName: 'Deliver before', width: 150, editable: true },
   { field: 'quantity', headerName: 'Quantity', width: 100, editable: true }
 ];
+
+export const DELIVERY_COLUMNS: GridColDef[] = [
+  { field: 'name', headerName: 'Name', width: 150, editable: true },
+  { field: 'phoneNumber', headerName: 'Phone number', width: 120, editable: true },
+  { field: 'street', headerName: 'Street', width: 150, editable: true },
+  { field: 'houseNumber', headerName: 'House number', width: 120, editable: true },
+  { field: 'city', headerName: 'City', width: 120, editable: true },
+  { field: 'country', headerName: 'Country', width: 150, editable: true },
+  { field: 'quantity', headerName: 'Quantity', width: 100, editable: true }
+];
