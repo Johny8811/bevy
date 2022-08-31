3a176482fe82cd6bb858752b8d2dbf33400fc97e
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-31 21:47:05 +0200

    Fetch data based on user role

--- src/main/modules/dashboard/hooks/useDeliveryTableData.ts
+++ src/main/modules/dashboard/hooks/useDeliveryTableData.ts
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
+import { OnfleetTask } from '@onfleet/node-onfleet/Resources/Tasks';
 
-import { useUserTasksQuery } from '../../../queryHooks/useUserTasksQuery';
+import { useTasksQuery } from '../../../queryHooks/useTasksQuery';
 import { useTasksTomorrowQuery } from '../../../queryHooks/useTasksTomorrowQuery';
-
 import { useHasRole } from '../../../integrations/firebase/hooks/useHasRole';
 import { useSnackBar } from '../../../components/snackBar/SnackbarProvider';
 import { TaskData } from '../../../types/tasks';
@@ -10,12 +10,11 @@ import { useUser } from '../../../integrations/firebase/components/UserProvider'
 
 export const useDeliveryTableData = (selectedDay: Date | null) => {
   const { user } = useUser();
-  const userTasksQuery = useUserTasksQuery();
-  const nextDayTasksQuery = useTasksTomorrowQuery();
-
-  const nextDayTasksQuery = useNextDayTasksQuery();
   const { openSnackBar } = useSnackBar();
   const hasRole = useHasRole();
+
+  const tasksQuery = useTasksQuery();
+  const tasksTomorrowQuery = useTasksTomorrowQuery();
 
   const [userTasks, setUserTasks] = useState<
     | ({ id: string } & Pick<
@@ -25,45 +24,46 @@ export const useDeliveryTableData = (selectedDay: Date | null) => {
     | []
   >([]);
 
-  useEffect(() => {
-    if (hasRole('dispatcher')) {
-      nextDayTasksQuery().then((tasks) => {
-        setUserTasks(
-          tasks.map((task) => ({
-            id: task.id,
-            name: task.recipients[0]?.name,
-            phoneNumber: task.recipients[0]?.phone,
-            street: task.destination.address.street,
-            houseNumber: task.destination.address.number,
-            city: task.destination.address.city,
-            country: task.destination.address.country,
-            quantity: task.quantity
-          }))
-        );
+  const transformTasks = (tasks: OnfleetTask[]) => {
+    return tasks.map((task) => ({
+      id: task.id,
+      name: task.recipients[0]?.name,
+      phoneNumber: task.recipients[0]?.phone,
+      street: task.destination.address.street,
+      houseNumber: task.destination.address.number,
+      city: task.destination.address.city,
+      country: task.destination.address.country,
+      quantity: task.quantity
+    }));
+  };
+
+  const handleFetchTasks = async () => {
+    try {
+      if (hasRole('dispatcher')) {
+        const tasks = await tasksTomorrowQuery();
+        setUserTasks(transformTasks(tasks));
+      }
+
+      if (hasRole('root') && selectedDay) {
+        const tasks = await tasksQuery(selectedDay);
+        setUserTasks(transformTasks(tasks));
+      }
+
+      if (hasRole('user') && selectedDay) {
+        const tasks = await tasksQuery(selectedDay, user?.uid);
+        setUserTasks(transformTasks(tasks));
+      }
+    } catch (e) {
+      // TODO: log error
+      openSnackBar({
+        text: `Something went wrong while getting data`,
+        severity: 'error'
       });
-    } else if (user?.uid && selectedDay) {
-      userTasksQuery(user?.uid, selectedDay)
-        .then((tasks) =>
-          setUserTasks(
-            tasks.map((task) => ({
-              id: task.id,
-              name: task.recipients[0]?.name,
-              phoneNumber: task.recipients[0]?.phone,
-              street: task.destination.address.street,
-              houseNumber: task.destination.address.number,
-              city: task.destination.address.city,
-              country: task.destination.address.country,
-              quantity: task.quantity
-            }))
-          )
-        )
-        .catch((e) => {
-          openSnackBar({
-            text: `Something went wrong while getting data - ${e.message}`,
-            severity: 'error'
-          });
-        });
     }
+  };
+
+  useEffect(() => {
+    handleFetchTasks();
   }, [selectedDay]);
 
   return userTasks;
