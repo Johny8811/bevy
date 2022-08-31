eaad53eee9528f651ed4bc6d712fd70d6ab83135
Jan Hujsa <jan.hujsa@gmail.com>
2022-08-31 15:46:31 +0200

    Squash

--- src/main/utils/buildUrlQueryParams.ts
+++ src/main/utils/buildUrlQueryParams.ts
@@ -1,11 +1,14 @@
-export type ParamsList = {
+type Param = {
   param: string;
   value: string | number | boolean;
-}[];
+};
+export type ParamsList = (Param | undefined)[];
 
 // TODO: add test
 export const buildUrlQueryParams = (paramsList: ParamsList) =>
-  paramsList.reduce((result, param) => {
-    const isResultEmpty = result.length !== 0;
-    return `${result}${isResultEmpty ? '&' : ''}${param.param}=${param.value}`;
-  }, '');
+  paramsList
+    .filter((v): v is Param => !!v)
+    .reduce((result, param) => {
+      const isResultEmpty = result.length !== 0;
+      return `${result}${isResultEmpty ? '&' : ''}${param.param}=${param.value}`;
+    }, '');
