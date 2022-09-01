dc70dba3370966790ee964df075b0c3cee6e63ad
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-01 12:38:59 +0200

    Reload page after exporting data to fetch exported tasks for next day

--- src/main/modules/dashboard/hooks/useOnFleetExportTasks.ts
+++ src/main/modules/dashboard/hooks/useOnFleetExportTasks.ts
@@ -12,6 +12,13 @@ export const useOnFleetExportTasks = () => {
         text: 'Tasks was successfully exported from onFleet.',
         severity: 'success'
       });
+      // TODO: after export is needed re-fetch data by "tasksTomorrowQuery". Currently we have our custom
+      //  implementation of queries, we have no option to re-fetch. After implementation of React-Query we can
+      //  re-fetch single query
+      setTimeout(() => {
+        // eslint-disable-next-line no-restricted-globals
+        location.reload();
+      }, 4000);
     } catch (e) {
       // TODO: log error
       openSnackBar({
