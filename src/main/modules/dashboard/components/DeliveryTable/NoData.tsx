c25cef70d6b39d964910088342c47d3f903ae8ba
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-04 14:27:44 +0200

    Change

--- src/main/modules/dashboard/components/DeliveryTable/NoData.tsx
+++ src/main/modules/dashboard/components/DeliveryTable/NoData.tsx
@@ -2,5 +2,5 @@ import React from 'react';
 import { GridOverlay } from '@mui/x-data-grid';
 
 export function NoData() {
-  return <GridOverlay>There is no data</GridOverlay>;
+  return <GridOverlay>No data</GridOverlay>;
 }
