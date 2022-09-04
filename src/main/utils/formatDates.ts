111b83498388412291fe887bcb95e021cfff5653
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-04 11:59:49 +0200

    Change format to date-time util to 24 hours format

--- src/main/utils/formatDates.ts
+++ src/main/utils/formatDates.ts
@@ -1,6 +1,6 @@
 import { format } from 'date-fns';
 
 export const formatToDateAndTime = (timestamp: number | Date) =>
-  format(timestamp, 'MM.dd.yyyy hh:mm');
+  format(timestamp, 'MM.dd.yyyy HH:mm');
 
 export const formatToTime = (timestamp: number | Date) => format(timestamp, 'hh:mm');
