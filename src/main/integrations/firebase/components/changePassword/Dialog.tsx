dd75fa7f51d191a1f91824e27e6ef8943b6177b2
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-06 22:00:11 +0200

    Add form to change password

--- src/main/integrations/firebase/components/changePassword/Dialog.tsx
+++ src/main/integrations/firebase/components/changePassword/Dialog.tsx
@@ -1,12 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@mui/material/Button';
 import MuiDialog from '@mui/material/Dialog';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
-import TextField from '@mui/material/TextField';
 import DialogActions from '@mui/material/DialogActions';
+import Box from '@mui/material/Box';
 
+import TextField from '@mui/material/TextField';
 import { ChangePasswordState } from '../UserProvider';
 
 type Props = {
@@ -15,29 +16,57 @@ type Props = {
 };
 
 export function Dialog({ changePasswordState, onCloseDialog }: Props) {
+  const [newPasswordError /* , setNewPasswordError */] = useState(false);
+  const [newPasswordAgainError /* , setNewPasswordAgainError */] = useState(false);
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  };
+
   return (
     <MuiDialog open={!!changePasswordState} onClose={onCloseDialog} fullWidth>
-      <DialogTitle>Change password</DialogTitle>
-      <DialogContent>
-        <DialogContentText>
-          {changePasswordState?.firstTime
-            ? 'You need to update your password because this is the first time you are singing in.'
-            : ''}
-        </DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="password"
-          label="Password"
-          type="password"
-          fullWidth
-          variant="standard"
-        />
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onCloseDialog}>Cancel</Button>
-        <Button onClick={() => {}}>Change password</Button>
-      </DialogActions>
+      <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+        <DialogTitle>Change password</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            {changePasswordState?.firstTime
+              ? 'You need to update your password because this is the first time you are singing in.'
+              : ''}
+          </DialogContentText>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="newPassword"
+            label="New Password"
+            type={showPassword ? 'text' : 'password'}
+            id="newPassword"
+            autoComplete="current-password"
+            error={newPasswordError}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="newPasswordAgain"
+            label="New Password Again"
+            type={showPassword ? 'text' : 'password'}
+            id="newPasswordAgain"
+            autoComplete="current-password"
+            error={newPasswordAgainError}
+            // helperText={'ble' || undefined}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setShowPassword((v) => !v)}>Show password</Button>
+          <Button onClick={onCloseDialog}>Cancel</Button>
+          <Button variant="contained" type="submit">
+            Change password
+          </Button>
+        </DialogActions>
+      </Box>
     </MuiDialog>
   );
 }
