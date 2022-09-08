5f950173b9be44b6700b63e88c95a71b1ca9a989
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-08 11:05:22 +0200

    Add query hook to update user info

---
+++ src/main/queryHooks/useUpdateUserInfoQuery.ts
@@ -0,0 +1,21 @@
+import { Methods } from '../integrations/fetch/fetchApi';
+import { useFetchBackend } from '../integrations/fetch/hooks/useFetchBackend';
+import { UPDATE_USER_INFO } from '../integrations/fetch/endpoints';
+
+type Body = {
+  userId: string;
+  displayName: string;
+  photoURL: string;
+};
+
+export const useUpdateUserInfoQuery = () => {
+  const fetchBackend = useFetchBackend();
+
+  return (body: Body) => {
+    return fetchBackend({
+      method: Methods.put,
+      url: UPDATE_USER_INFO,
+      body
+    });
+  };
+};
