d0668685ef04d55a57f0cfbf023f6db5b97b706c
Jan Hujsa <jan.hujsa@gmail.com>
2022-09-20 19:21:50 +0200

    Add dark mode switching based on os system

--- src/main/App.tsx
+++ src/main/App.tsx
@@ -3,6 +3,8 @@ import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { red } from '@mui/material/colors';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import CssBaseline from '@mui/material/CssBaseline';
 
 import { SnackBarProvider } from './components/snackBar/SnackbarProvider';
 import { LoadingProvider } from './integrations/fetch/components/LoadingProvider';
@@ -11,24 +13,32 @@ import { UserProvider } from './integrations/firebase/components/UserProvider';
 import { DialogProvider } from './components/dialogProvider/DialogProvider';
 import Routing from './routing/Routing';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#318260'
-    },
-    secondary: {
-      main: '#b72548'
-    },
-    error: {
-      main: red.A400
-    }
-  }
-});
-
 export function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+
+  const theme = React.useMemo(
+    () =>
+      createTheme({
+        palette: {
+          primary: {
+            main: '#318260'
+          },
+          secondary: {
+            main: '#b72548'
+          },
+          error: {
+            main: red.A400
+          },
+          mode: prefersDarkMode ? 'dark' : 'light'
+        }
+      }),
+    [prefersDarkMode]
+  );
+
   return (
     <React.StrictMode>
       <ThemeProvider theme={theme}>
+        <CssBaseline />
         <LocalizationProvider dateAdapter={AdapterDateFns}>
           <SnackBarProvider>
             <LoadingProvider>
