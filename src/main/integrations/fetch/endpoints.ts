1893b254c810b6ea06ecc5995a6bb5e90b6482a5
Jan Hujsa <jan.hujsa@gmail.com>
2022-10-16 18:36:53 +0200

    Change dev endpoint

--- src/main/integrations/fetch/endpoints.ts
+++ src/main/integrations/fetch/endpoints.ts
@@ -6,8 +6,8 @@ const ON_FLEET_URL = 'https://onfleet.com/api/v2';
 export const ON_FLEET_TASKS_BATCH_URL = `${ON_FLEET_URL}/tasks/batch`;
 export const ON_FLEET_WORKERS = `${ON_FLEET_URL}/workers`;
 
-// Our BE
-const BE_URL_DEV = 'http://localhost:5001/bevy-c8798/us-central1/tasks';
+// Backend
+const BE_URL_DEV = 'http://localhost:5001/bevy-dev/us-central1/tasks';
 const BE_URL_PROD = 'https://us-central1-bevy-c8798.cloudfunctions.net/tasks';
 const BASE_URL = isDev() ? BE_URL_DEV : BE_URL_PROD;
 
