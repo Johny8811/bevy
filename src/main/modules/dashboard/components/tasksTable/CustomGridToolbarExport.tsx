ad58788af225d618a94d951fbd293b21e6491339
Jan Hujsa <jan.hujsa@gmail.com>
2023-02-01 21:17:38 +0100

    Add button to download aggregated tasks for NutritionPro

--- src/main/modules/dashboard/components/tasksTable/CustomGridToolbarExport.tsx
+++ src/main/modules/dashboard/components/tasksTable/CustomGridToolbarExport.tsx
@@ -12,6 +12,7 @@ import { writeFileXLSX, utils } from 'xlsx';
 
 import { formatToDateAndTime } from '../../../../utils/formatDates';
 import { getGridData } from '../../../../utils/dataGrid/getGridData';
+import { useAggregatedTasksQuery } from '../../../../queryHooks/useAggregatedTasksQuery';
 
 function ExcelExportMenuItem(props: GridExportMenuItemProps<{}>) {
   const apiRef = useGridApiContext();
@@ -45,12 +46,85 @@ function ExcelExportMenuItem(props: GridExportMenuItemProps<{}>) {
   );
 }
 
+function AggregatedAddressExportMenuItem() {
+  const getAggregatedTasks = useAggregatedTasksQuery();
+
+  return (
+    <MenuItem
+      onClick={() => {
+        getAggregatedTasks().then((data) => {
+          const formattedData = data
+            // @ts-ignore
+            .map((d) => [
+              {
+                name: undefined,
+                phoneNumber: undefined,
+                // eslint-disable-next-line no-underscore-dangle
+                street: d._id.street,
+                // eslint-disable-next-line no-underscore-dangle
+                number: d._id.number,
+                // eslint-disable-next-line no-underscore-dangle
+                city: d._id.city,
+                // eslint-disable-next-line no-underscore-dangle
+                completeAfter: formatToDateAndTime(d._id.completeAfter),
+                completeBefore: undefined,
+                country: undefined,
+                postalCode: undefined,
+                shortId: undefined,
+                deliveredAt: undefined,
+                estimatedArrivalTime: undefined,
+                order: undefined,
+                quantity: undefined,
+                recipientNotes: undefined,
+                slot: undefined,
+                workerName: undefined,
+                workerPhone: undefined
+              },
+              // @ts-ignore
+              d.uniqAddress.map((a) => ({
+                name: a.recipients[0]?.name,
+                phoneNumber: a.recipients[0]?.phoneNumber,
+                street: a.destination.address.street,
+                number: a.destination.address.number,
+                city: a.destination.address.city,
+                completeAfter: formatToDateAndTime(a.completeAfter),
+                completeBefore: formatToDateAndTime(a.completeBefore),
+                country: a.destination.address.country,
+                postalCode: a.destination.address.postalCode,
+                shortId: a.shortId,
+                deliveredAt: a.deliveredAt,
+                estimatedArrivalTime: formatToDateAndTime(a.estimatedArrivalTime),
+                order: a.order,
+                quantity: a.quantity,
+                recipientNotes: a.recipients[0]?.recipientNotes,
+                slot: a.slot,
+                workerName: undefined,
+                workerPhone: undefined
+              }))
+            ])
+            .flat(2);
+
+          const worksheet = utils.json_to_sheet(formattedData);
+          const workbook = {
+            Sheets: { Tasks: worksheet },
+            SheetNames: ['Tasks']
+          };
+
+          writeFileXLSX(workbook, 'BEVY_NP_aggregated.xlsx');
+        });
+      }}>
+      Download aggregated tasks
+    </MenuItem>
+  );
+}
+
 export function CustomGridToolbarExport(props: ButtonProps) {
   return (
     <GridToolbarExportContainer {...props}>
       <GridCsvExportMenuItem />
       <ExcelExportMenuItem />
       <GridPrintExportMenuItem />
+      <AggregatedAddressExportMenuItem />
     </GridToolbarExportContainer>
   );
 }
