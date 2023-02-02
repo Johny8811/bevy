c6d4e6f0bf11b4d08b92069e5fa0c6798e4cdcf8
Jan Hujsa <jan.hujsa@gmail.com>
2023-02-02 14:39:02 +0100

    Fix

--- src/main/modules/dashboard/components/tasksTable/CustomGridToolbarExport.tsx
+++ src/main/modules/dashboard/components/tasksTable/CustomGridToolbarExport.tsx
@@ -55,66 +55,77 @@ function AggregatedAddressExportMenuItem(dateRange: DateRange) {
   return (
     <MenuItem
       onClick={() => {
-        getAggregatedTasks().then((data) => {
-          const formattedData = data
-            // @ts-ignore
-            .map((d) => [
-              {
-                name: undefined,
-                phoneNumber: undefined,
-                // eslint-disable-next-line no-underscore-dangle
-                street: d._id.street,
-                // eslint-disable-next-line no-underscore-dangle
-                number: d._id.number,
-                // eslint-disable-next-line no-underscore-dangle
-                city: d._id.city,
-                // eslint-disable-next-line no-underscore-dangle
-                completeAfter: formatToDateAndTime(d._id.completeAfter),
-                completeBefore: undefined,
-                country: undefined,
-                postalCode: undefined,
-                shortId: undefined,
-                deliveredAt: undefined,
-                estimatedArrivalTime: undefined,
-                order: undefined,
-                quantity: undefined,
-                recipientNotes: undefined,
-                slot: undefined,
-                workerName: undefined,
-                workerPhone: undefined
-              },
+        getAggregatedTasks()
+          .then((data) => {
+            const formattedData = data
               // @ts-ignore
-              d.uniqAddress.map((a) => ({
-                name: a.recipients[0]?.name,
-                phoneNumber: a.recipients[0]?.phoneNumber,
-                street: a.destination.address.street,
-                number: a.destination.address.number,
-                city: a.destination.address.city,
-                completeAfter: formatToDateAndTime(a.completeAfter),
-                completeBefore: formatToDateAndTime(a.completeBefore),
-                country: a.destination.address.country,
-                postalCode: a.destination.address.postalCode,
-                shortId: a.shortId,
-                deliveredAt: a.deliveredAt,
-                estimatedArrivalTime: formatToDateAndTime(a.estimatedArrivalTime),
-                order: a.order,
-                quantity: a.quantity,
-                recipientNotes: a.recipients[0]?.recipientNotes,
-                slot: a.slot,
-                workerName: undefined,
-                workerPhone: undefined
-              }))
-            ])
-            .flat(2);
+              .map((d) => [
+                {
+                  name: undefined,
+                  phoneNumber: undefined,
+                  // eslint-disable-next-line no-underscore-dangle
+                  street: d._id.street,
+                  // eslint-disable-next-line no-underscore-dangle
+                  number: d._id.number,
+                  // eslint-disable-next-line no-underscore-dangle
+                  city: d._id.city,
+                  // eslint-disable-next-line no-underscore-dangle
+                  completeAfter: d._id.completeAfter
+                    ? // eslint-disable-next-line no-underscore-dangle
+                      formatToDateAndTime(d._id.completeAfter)
+                    : undefined,
+                  completeBefore: undefined,
+                  country: undefined,
+                  postalCode: undefined,
+                  shortId: undefined,
+                  deliveredAt: undefined,
+                  estimatedArrivalTime: undefined,
+                  order: undefined,
+                  quantity: undefined,
+                  recipientNotes: undefined,
+                  slot: undefined,
+                  workerName: undefined,
+                  workerPhone: undefined
+                },
+                // @ts-ignore
+                d.uniqAddress.map((a) => ({
+                  name: a.recipients[0]?.name,
+                  phoneNumber: a.recipients[0]?.phoneNumber,
+                  street: a.destination.address.street,
+                  number: a.destination.address.number,
+                  city: a.destination.address.city,
+                  completeAfter: a.completeAfter ? formatToDateAndTime(a.completeAfter) : undefined,
+                  completeBefore: a.completeBefore
+                    ? formatToDateAndTime(a.completeBefore)
+                    : undefined,
+                  country: a.destination.address.country,
+                  postalCode: a.destination.address.postalCode,
+                  shortId: a.shortId,
+                  deliveredAt: a.deliveredAt,
+                  estimatedArrivalTime: a.estimatedArrivalTime
+                    ? formatToDateAndTime(a.estimatedArrivalTime)
+                    : undefined,
+                  order: a.order,
+                  quantity: a.quantity,
+                  recipientNotes: a.recipients[0]?.recipientNotes,
+                  slot: a.slot,
+                  workerName: undefined,
+                  workerPhone: undefined
+                }))
+              ])
+              .flat(2);
 
-          const worksheet = utils.json_to_sheet(formattedData);
-          const workbook = {
-            Sheets: { Tasks: worksheet },
-            SheetNames: ['Tasks']
-          };
+            const worksheet = utils.json_to_sheet(formattedData);
+            const workbook = {
+              Sheets: { Tasks: worksheet },
+              SheetNames: ['Tasks']
+            };
 
-          writeFileXLSX(workbook, 'BEVY_NP_aggregated.xlsx');
-        });
+            writeFileXLSX(workbook, 'BEVY_NP_aggregated.xlsx');
+          })
+          .catch((e) => {
+            console.error('==> Error: ', e);
+          });
       }}>
       Download aggregated tasks
     </MenuItem>
